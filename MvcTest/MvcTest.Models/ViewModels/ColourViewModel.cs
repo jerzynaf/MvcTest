82edf028402f48dbc058d97e2d52d30bc8559cea
jerzynaf <filip.jerzyna"answerconsulting.com>
2015-12-24 10:08:02 +0000

    ViewModels have been added.

---
+++ MvcTest/MvcTest.Models/ViewModels/ColourViewModel.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcTest.Models.ViewModels
+{
+    public class ColourViewModel
+    {
+        public int ColourId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        public bool IsEnabled { get; set; }
+    }
+}
---
+++ MvcTest/MvcTest.Models/ViewModels/PersonViewModel.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcTest.Models.ViewModels
+{
+    public partial class Person
+    {
+        public int PersonId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        public bool IsAuthorised { get; set; }
+
+        public bool IsValid { get; set; }
+
+        public bool IsEnabled { get; set; }
+
+        public virtual ICollection<ColourViewModel> Colours { get; set; }
+    }
+}
