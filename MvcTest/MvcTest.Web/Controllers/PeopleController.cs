30b0f9831ed5db21bd3624e8536cbb89de4673c6
jerzynaf <filip.jerzyna"answerconsulting.com>
2016-01-20 13:08:15 +0000

    Bootstrap styling added.

--- MvcTest/MvcTest.Web/Controllers/PeopleController.cs
+++ MvcTest/MvcTest.Web/Controllers/PeopleController.cs
@@ -18,33 +18,36 @@ namespace MvcTest.Web.Controllers
             _colourRepository = colourRepository;
         }
 
-        // GET: People
+        [HttpGet]
         public ActionResult Index()
         {
             var people = _personRepository.GetAllPeople();
             var peopleViewModels = Mapper.Map<List<PersonViewModel>>(people);
+
             return View(peopleViewModels);
         }
 
-        // GET: People/Edit/5
+        [HttpGet]
         public ActionResult Edit(int id)
         {
             var person = _personRepository.GetPerson(id);
+
             if (person == null)
             {
                 return HttpNotFound();
             }
+
             var personViewModel = Mapper.Map<PersonViewModel>(person);
-            var allColours = _colourRepository.GetAllColours().OrderBy(c=>c.Name);
+            var allColours = _colourRepository.GetAllColours().OrderBy(c => c.Name);
             var allColourViewModels = Mapper.Map<List<ColourViewModel>>(allColours);
             var favouriteColourIds = person.Colours.Select(c => c.ColourId);
+
             personViewModel.Colours.Clear();
             personViewModel = LoadColourViewModels(personViewModel, favouriteColourIds, allColourViewModels);
 
             return View(personViewModel);
         }
 
-        // POST: People/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit(PersonViewModel personViewModel)
@@ -54,16 +57,20 @@ namespace MvcTest.Web.Controllers
                 UpdatePerson(personViewModel);
                 return RedirectToAction("Index");
             }
+
             return View(personViewModel);
         }
 
         private void UpdatePerson(PersonViewModel personViewModel)
         {
             var person = _personRepository.GetPerson(personViewModel.PersonId);
+
             person.IsAuthorised = personViewModel.IsAuthorised;
             person.IsEnabled = personViewModel.IsEnabled;
             person.Colours.Clear();
+
             var pickedColours = personViewModel.Colours.Where(c => c.IsChecked == true).Select(c => c.ColourId);
+
             for (int i = 0; i < pickedColours.Count(); i++)
             {
                 var colour = _colourRepository.GetColour(pickedColours.ElementAt(i));
@@ -78,12 +85,15 @@ namespace MvcTest.Web.Controllers
             for (int i = 0; i < allColourViewModels.Count; i++)
             {
                 var tempColour = allColourViewModels.ElementAt(i);
+
                 if (favouriteColourIds.Contains(tempColour.ColourId))
                 {
                     tempColour.IsChecked = true;
                 }
+
                 personViewModel.Colours.Add(tempColour);
             }
+
             return personViewModel;
         }
     }
