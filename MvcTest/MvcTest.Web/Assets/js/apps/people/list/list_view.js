223b8d4b91823e357520ca55601210272ed660df
jerzynaf <filip.jerzyna"answerconsulting.com>
2016-01-24 18:03:17 +0000

    list_view-module for views.

--- MvcTest/MvcTest.Web/Assets/js/apps/people/list/list_view.js
+++ MvcTest/MvcTest.Web/Assets/js/apps/people/list/list_view.js
@@ -1,1 +1,19 @@
-﻿
+﻿PeopleManager.module("PeopleApp.List", function(List, PeopleManager, Backbone, Marionette, $, _) {
+  List.PersonView = Marionette.ItemView.extend({
+    template: "#person-template",
+
+    events: {
+      "click a": "alertFirstName"
+    },
+
+    alertFirstName: function () {
+      alert(this.model.escape("firstName"));
+    },
+    tagName: "li"
+  });
+
+  List.PeopleView = Marionette.CollectionView.extend({
+    tagName: "ul",
+    childView: List.PersonView
+});
+});
