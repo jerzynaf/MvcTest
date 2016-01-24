aee1c0375049ba41d3d853afc0031d4903b39b6f
jerzynaf <filip.jerzyna"answerconsulting.com>
2016-01-24 18:08:13 +0000

    Marionette- list_controller added.

--- MvcTest/MvcTest.Web/Assets/js/apps/people/list/list_controller.js
+++ MvcTest/MvcTest.Web/Assets/js/apps/people/list/list_controller.js
@@ -1,1 +1,13 @@
-﻿
+﻿PeopleManager.module("PeopleApp.List", function(List, PeopleManager, Backbone, Marionette, $, _) {
+  List.Controller = {
+    listPeople: function() {
+      var people = PeopleManager.request("person:entities");
+
+      var peopleListView = new List.PeopleView({
+        collection: people
+      });
+
+      PeopleManager.mainRegion.show(peopleListView);
+    }
+  };
+});
