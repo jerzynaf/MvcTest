f3f4e1b97c88faf9e75923bf22a45df95056d910
jerzynaf <filip.jerzyna"answerconsulting.com>
2016-01-24 19:51:59 +0000

    Defaulr route added

--- MvcTest/MvcTest.Web/Assets/js/app.js
+++ MvcTest/MvcTest.Web/Assets/js/app.js
@@ -4,9 +4,22 @@ PeopleManager.addRegions({
   mainRegion: "#main-region"
 });
 
+PeopleManager.navigate = function (route, options) {
+  options || (options = {});
+  Backbone.history.navigate(route, options);
+};
+
+PeopleManager.getCurrentRoute = function () {
+  return Backbone.history.fragment;
+};
+
 PeopleManager.on("start", function () {
   if (Backbone.history) {
     Backbone.history.start();
   }
-  //PeopleManager.PeopleApp.List.Controller.listPeople();
+
+  if (Backbone.history.fragment === "") {
+    Backbone.history.navigate("people");
+    PeopleManager.PeopleApp.List.Controller.listPeople();
+  }
 });
